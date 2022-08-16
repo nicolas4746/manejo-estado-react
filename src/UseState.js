01d0b07d3318ed5bda67bb9c6da1648eb571d315
Nicolas Goitia <nicolasgoitia10@gmail.com>
2022-08-16 10:16:44 -0300

    diferencias manejo estado con funciones o clases,basico

--- src/UseState.js
+++ src/UseState.js
@@ -1,15 +1,24 @@
 import React from 'react';
 
-function UseState() {
+function UseState({ name }) {
+    const [error, setError] = React.useState(true);
+  
     return (
-        <div>
-            <h2>Eliminar UseState</h2>
-        
-            <p>Por favor, escribe el codigo de seguridad.</p>
-            
-            <input placeholder="Codigo de seguridad"/>
-            <button>Comprobar</button>
-        </div>
+      <div>
+        <h2>Eliminar {name}</h2>
+  
+        <p>Por favor, escribe el código de seguridad.</p>
+  
+        {error && (
+          <p>Error: el código es incorrecto</p>
+        )}
+  
+        <input placeholder="Código de seguridad" />
+        <button
+          onClick={() => setError(!error)}
+        >Comprobar</button>
+      </div>
     );
-}
-export {UseState};
+  }
+
+export { UseState };
